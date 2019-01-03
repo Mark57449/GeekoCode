8aee40a45eedffcd88319451f051a969a5a2d04a
Mark57449 <wendel.souza2@etec.sp.gov.br>
2019-01-03 12:51:17 -0200

    Codificação do Footer, adição do widget do footer.

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php wp_footer(); ?>
+
+	<footer class="container-fluid bg-danger border-dark p-4">
+		<div class="container">
+			<div class="row">
+				<div class="col-4">
+					<img class="img-fluid logoFooter pr-5 pl-2" src="<?php bloginfo('template_url'); ?>/source/img/GeekoWhite.png">
+					<p class="texto text-white text-justify px-3 mt-2"><?php echo get_theme_mod('texto_footer_companhia'); ?></p>
+				</div>
+		         <?php //Adiciona um formulário de buscas
+		            dynamic_sidebar('Footer');
+		           ?>
+			</div>
+		</div>
+	</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php bloginfo('template_url'); ?>/source/js/jquery.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/source/js/popper.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/source/js/bootstrap.js"></script>
+  </body>
+</html>
