693b77381c95ac9f3384c6d33d08afc8c79b786d
Mark57449 <wendel.souza2@etec.sp.gov.br>
2019-01-11 19:27:30 -0200

    Comentários desnecessários removidos; alterações de margin e padding.

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
     <header>
       
-    <section id="GO_ExtraCabecalho" class="container-fluid bg-danger p-1 align-items-center">
-      <div class="container d-flex justify-content-between">
-        <a href="<?php echo get_theme_mod('extracabelhado_title_link', 'www.geeko.com.br'); ?>"><h6 class="pt-1 m-0"><?php echo get_theme_mod('extracabelhado_title', 'A Geeko! foi criada com muito esforço e dedicação!'); ?></h6></a>
+    <section id="GO_ExtraCabecalho" class="container-fluid bg-danger p-2 align-items-center shadow-sm">
+      <div class="container d-flex justify-content-center">
+        <!-- <a href="<?php //echo get_theme_mod('extracabelhado_title_link', 'www.geeko.com.br'); ?>"><h6 class="pt-1 m-0"><?php //echo get_theme_mod('extracabelhado_title', 'A Geeko! foi criada com muito esforço e dedicação!'); ?></h6></a> -->
         <nav class="d-flex justify-content-between">
           <a href="#"><i class="fab fa-instagram text-white ml-2"></i></a>
           <a href="#"><i class="fab fa-youtube text-white ml-2"></i></a>
@@ -14,15 +14,15 @@
       </div>
     </section> 
 
-    <div id="GO_Cabecalho" class="container-fluid p-3 bg-gray shadow-sm" style="background: linear-gradient(rgba(0,0,0,0.05), rgba(0,0,0,0.5)), url('<?php echo get_theme_mod('cabecalho_url_image'); ?>');">
-      <div class="container px-0 mt-5">
+    <div id="GO_Cabecalho" class="container-fluid p-2 mb-2 bg-gray shadow-sm" style="background: linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.6)), url('<?php echo get_theme_mod('cabecalho_url_image'); ?>');">
+      <div class="container-fluid px-0 mt-5">
         <div class="row align-items-center d-flex justify-content-center my-1">
           <div id="GO_IdentidadeGeeko">
             <a href="<?php bloginfo('url'); ?>">
               <?php if(get_header_image() == '') { ?>
               <h1><?php bloginfo('name'); ?></h1>
               <?php } else { ?>
-              <img class="LogoGeeko" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Esse é logo da Geeko!" />
+              <img class="LogoGeeko img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Esse é logo da Geeko!" />
               <?php } ?>
             </a>
             
@@ -42,47 +42,26 @@
 
               <div class="d-flex">
                 <?php
-                  wp_nav_menu( array(
-                    'theme_location'    => 'cabecalho',
-                    'depth'             => 2,
-                    'container'         => false,
-                    'menu_class'        => 'nav',
-                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                    'walker'            => new WP_Bootstrap_Navwalker(),
-                  ) );
+                  // wp_nav_menu( array(
+                  //   'theme_location'    => 'cabecalho',
+                  //   'depth'             => 2,
+                  //   'container'         => false,
+                  //   'menu_class'        => 'nav',
+                  //   'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                  //   'walker'            => new WP_Bootstrap_Navwalker(),
+                  // ) );
                 ?>
-                <button type="button" class="btn rounded p-0 shadow" style="background: #e6763b;">
+                <button type="button" class="d-none btn rounded p-0 shadow" style="background: #e6763b;">
                   <a href="#" class="nav-link text-light py-0">Seguir</a>
                 </button>
                 <div class="d-none">
                   <?php //Adiciona um formulário de buscas
-                    dynamic_sidebar('Busca');
+                    //dynamic_sidebar('Busca');
                   ?>
                 </div>
               </div>
         </nav>
       </div>
     </div>
-
-        <!-- <nav class="navbar navbar-expand-lg bg-danger rounded navbar-dark mb-1 role="navigation">
-          <div class="container">
-          
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-            <?php
-            /*wp_nav_menu( array(
-              'theme_location'    => 'principal',
-              'depth'             => 2,
-              'container'         => 'div',
-              'container_class'   => 'collapse navbar-collapse',
-              'container_id'      => 'bs-example-navbar-collapse-1',
-              'menu_class'        => 'nav navbar-nav',
-              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-              'walker'            => new WP_Bootstrap_Navwalker(),
-            ) );*/
-            ?>
-          </div>
-        </nav> -->
 
       </header>
